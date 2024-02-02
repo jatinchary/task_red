8e678e814621977d1d9474914649ce4d1df62eac
nagendra <narrennagendra@gmail.com>
2024-02-02 13:51:14 +0530

    added scraper

--- scraper.js
+++ scraper.js
@@ -1,33 +1,35 @@
-// scraper.js
 const axios = require('axios');
 const cheerio = require('cheerio');
-const NewsItem = require('./models/newsItem');
 
-async function scrapeAndStore() {
-  const url = 'https://news.ycombinator.com/';
-  const response = await axios.get(url);
-  const $ = cheerio.load(response.data);
+const baseUrl = "https://news.ycombinator.com/";
+const pagesToScrape = 3;
 
-  const newsItems = [];
+async function getData() {
+	try {
+		for (let i = 1; i <= pagesToScrape; i++) {
+            const url = `${baseUrl}?p=${i}`;
 
-  $('tr.athing').each((index, element) => {
-    const title = $(element).find('a.storylink').text();
-    const url = $(element).find('a.storylink').attr('href');
-    const hackerNewsUrl = `${url}item?id=${$(element).attr('id')}`;
-    const upvotes = parseInt($(element).next('tr').find('span.score').text().split(' ')[0]) || 0;
-    const comments = parseInt($(element).next('tr').find('a').last().text().split(' ')[0]) || 0;
+            const response = await axios.get(url);
+            const html = response.data;
 
-    newsItems.push({
-      url,
-      hackerNewsUrl,
-      upvotes,
-      comments
-    });
-  });
+            const $ = cheerio.load(html);
+			const tbody = $('#hnmain > tbody > tr:nth-child(3) > td > table > tbody');
+			const children = tbody.children('tr');
 
-  for (const item of newsItems) {
-    await NewsItem.findOneAndUpdate({ url: item.url }, { $set: item }, { upsert: true, new: true });
-  }
+			for (let ind = 0; ind < children.length-2; ind=ind+3) {
+				const rank = $(children[ind]).find('> td:nth-child(1) > span').text().slice(0, -1);
+				const title = $(children[ind]).find('> td:nth-child(3) > span > a').text();
+				const url = $(children[ind]).find('> td:nth-child(3) > span > a').attr('href');
+				const upvotes = $(children[ind+1]).find('td.subtext > span > span.score').text().split(' ')[0];
+				const posted_on = $(children[ind+1]).find('td.subtext > span > span.age').attr('title');
+				const number_of_comments = $(children[ind+1]).find('> td.subtext > span > a:last-child').text().trim();
+			}
+        }
+
+	} catch (error) {
+		console.log(error);
+	}
+
 }
 
-module.exports = scrapeAndStore;
+getData();
